4fafa93f5d06370678061368e9730b4576053bdc
Pilot.Phil <632913013@qq.com>
2021-09-25 22:42:21 +0800

    up temp version

---
+++ PilotSoft.Serial/PilotSoft.Serial/SerialPortFactory.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace PilotSoft.Serial
+{
+    public class SerialPortFactory
+    {
+        /// <summary>
+        /// 快速创建9600波特率串口对象 常用
+        /// </summary>
+        /// <param name="portName">端口名，如COM12</param>
+        /// <returns></returns>
+        public SerialPort QuickCreateSerialPort9600(string portName)
+        {
+            return new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+        }
+
+        /// <summary>
+        /// 快速创建19200波特率串口对象 常用
+        /// </summary>
+        /// <param name="portName">端口名，如COM12</param>
+        /// <returns></returns>
+        public SerialPort QuickCreateSerialPort19200(string portName)
+        {
+            return new SerialPort(portName, 19200, Parity.None, 8, StopBits.One);
+        }
+
+        /// <summary>
+        /// 快速创建串口对象 常用
+        /// </summary>
+        /// <param name="portName">端口名，如：COM12</param>
+        /// <param name="baudRate">波特率，如：9600</param>
+        /// <returns></returns>
+        public SerialPort QuickCreateSerialPort19200(string portName, int baudRate)
+        {
+            return new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+        }
+
+        /// <summary>
+        /// 通过字符串参数创建串口对象
+        /// </summary>
+        /// <param name="portName"></param>
+        /// <param name="baudrate"></param>
+        /// <param name="parity"></param>
+        /// <param name="databits"></param>
+        /// <param name="stopbits"></param>
+        /// <returns></returns>
+        public SerialPort CreateSerialPortNormal(string portName,
+                                                      int baudrate,
+                                                      string parity,
+                                                      int databits,
+                                                      string stopbits)
+        {
+            var serialPort = new SerialPort();
+
+            try
+            {
+                serialPort.PortName = portName;
+                serialPort.BaudRate = baudrate;
+
+                serialPort.Parity = new Func<Parity>(() =>
+                {
+                    if (parity == "None" || parity == "none" || parity == "无")
+                        return Parity.None;
+                    else if (parity == "Odd" || parity == "odd" || parity == "奇")
+                        return Parity.Odd;
+                    else if (parity == "Even" || parity == "even" || parity == "偶")
+                        return Parity.Even;
+                    else if (parity == "Space" || parity == "space" || parity == "0")
+                        return Parity.Space;
+                    else if (parity == "Mark" || parity == "mark" || parity == "1")
+                        return Parity.Mark;
+                    else
+                        throw new Exception("Parity Param Not Right Exception from pilot.serial");
+                }).Invoke();
+
+                serialPort.DataBits = databits;
+
+                serialPort.StopBits = new Func<StopBits>(() =>
+                {
+                    if (stopbits == "None" || stopbits == "none" || stopbits == "无")
+                        return StopBits.None;
+                    else if (stopbits == "One" || stopbits == "one" || stopbits == "1")
+                        return StopBits.One;
+                    else if (stopbits == "Two" || stopbits == "two" || stopbits == "2")
+                        return StopBits.Two;
+                    else if (stopbits == "OnePointFive" || stopbits == "onepointfive" || stopbits == "1.5")
+                        return StopBits.OnePointFive;
+                    else
+                        throw new Exception("Stopbits Param Not Right Exception from pilot.serial");
+                }).Invoke();
+            }
+            catch
+            {
+                throw;
+            }
+            return serialPort;
+        }
+
+        /// <summary>
+        /// 通过字符串参数创建串口对象
+        /// </summary>
+        /// <param name="portName"></param>
+        /// <param name="baudrate"></param>
+        /// <param name="parity"></param>
+        /// <param name="databits"></param>
+        /// <param name="stopbits"></param>
+        /// <returns></returns>
+        public SerialPort CreateSerialPortByString(string portName,
+                                                        string baudrate,
+                                                        string parity,
+                                                        string databits,
+                                                        string stopbits)
+        {
+            return CreateSerialPortNormal(portName, int.Parse(baudrate), parity, int.Parse(databits), stopbits);
+        }
+    }
+}
---
+++ PilotSoft.Serial/PilotSoft.Serial/SerialUtil.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+
+namespace PilotSoft.Serial
+{
+    public class SerialUtil
+    {
+        private readonly SerialPort serialPort;
+        public bool IsOpen { get; set; } = false;
+
+        /// <summary>
+        /// 获取可用串口名
+        /// </summary>
+        /// <returns></returns>
+        public static string[] GetPortNameList()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="serialPort"></param>
+        public SerialUtil(SerialPort serialPort)
+        {
+            this.serialPort = serialPort;
+        }
+
+        /// <summary>
+        /// 打开串口
+        /// </summary>
+        /// <returns>操作是否成功</returns>
+        public bool OpenPort()
+        {
+            try
+            {
+                if (serialPort.IsOpen == true)
+                {
+                    serialPort.Close();
+                }
+                serialPort.Open();
+                IsOpen = true;
+            }
+            catch
+            {
+                IsOpen = false;
+                throw;
+            }
+            return IsOpen;
+        }
+
+        /// <summary>
+        /// 关闭串口
+        /// </summary>
+        public void ClosePort()
+        {
+            if(serialPort.IsOpen==true)
+            {
+                serialPort.Close();
+            }
+        }
+
+
+    }
+}
